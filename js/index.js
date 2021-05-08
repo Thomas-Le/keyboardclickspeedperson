19ea593d59cee2e065cc3e4db4e1bc83cbfc60a1
Thomas-Le <elsamoht@live.unc.edu>
2021-05-08 05:05:09 -0400

    Forgot to make function using await async

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@
         $('nav').load('./html/navigation.html', function() {
             if (loggedIn) {
                 $('nav').append('<input type="button" value="Log Out" id="logout"/>');
-                $root.on('click', '#logout', function() {
+                $root.on('click', '#logout', async function() {
                     await logout();
                     location.reload();
                 });
